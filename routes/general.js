c55f3ff94443c52e50490a2ba6814f964d851ffe
RajavelmuruganYS <ysrajavelmurugan@gmail.com>
2026-01-14 13:51:31 +0530

    Improve general.js with full async axios functions

--- routes/general.js
+++ routes/general.js
@@ -1,30 +1,41 @@
 const axios = require("axios");
 
-// Get all books
 async function getAllBooks() {
-  const response = await axios.get("http://localhost:3000/books");
-  console.log("All Books:", response.data);
+  try {
+    const res = await axios.get("http://localhost:3000/books");
+    console.log(res.data);
+  } catch (err) {
+    console.error(err.message);
+  }
 }
 
-// Get book by ISBN
 async function getBookByISBN(isbn) {
-  const response = await axios.get(`http://localhost:3000/books/isbn/${isbn}`);
-  console.log("Book by ISBN:", response.data);
+  try {
+    const res = await axios.get(`http://localhost:3000/books/isbn/${isbn}`);
+    console.log(res.data);
+  } catch (err) {
+    console.error(err.message);
+  }
 }
 
-// Get books by author
 async function getBooksByAuthor(author) {
-  const response = await axios.get(`http://localhost:3000/books/search?author=${author}`);
-  console.log("Books by Author:", response.data);
+  try {
+    const res = await axios.get(`http://localhost:3000/books/search?author=${author}`);
+    console.log(res.data);
+  } catch (err) {
+    console.error(err.message);
+  }
 }
 
-// Get books by title
 async function getBooksByTitle(title) {
-  const response = await axios.get(`http://localhost:3000/books/search?title=${title}`);
-  console.log("Books by Title:", response.data);
+  try {
+    const res = await axios.get(`http://localhost:3000/books/search?title=${title}`);
+    console.log(res.data);
+  } catch (err) {
+    console.error(err.message);
+  }
 }
 
-// Call functions
 getAllBooks();
 getBookByISBN("978-3-16-148410-0");
 getBooksByAuthor("Fitzgerald");
